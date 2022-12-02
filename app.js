bcd94a5f8fc92b840452d9be129d64c2a06514e0
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-02 23:09:15 +0300

    feat(UR): add test routes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
+import path from "path";
 import express from "express";
 import { dataBase } from "./db.js";
+import router from "./routes/userRoutes";
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use("/api", router);
 
 const start = async () => {
   try {
